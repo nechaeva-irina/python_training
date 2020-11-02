5ae3585a8ea7b7aea9351c06509a177886d0d978
Irina Nechaeva <ivn@tut.by>
2020-11-02 19:05:04 +0300

    Создана общая фикстура для всех тестов

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from fixture.application import Application
-#from fixture.application2 import Application2
 
 
 @pytest.fixture(scope = "session")
@@ -8,9 +7,3 @@ def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
-
-#def app2(request):
-  #  fixture = Application2()
-   # request.addfinalizer(fixture.destroy2)
-  #  return fixture
-
