d0f7b39d505f8eaa4fa059f87cf1150e8a11f1a3
Irina Nechaeva <ivn@tut.by>
2020-11-25 13:20:42 +0300

    Параметризированы тесты для создания контактов (assertion error)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    Contact(firstname=random_string("firstname", 15), middlename=random_string("middletname", 15),
+            lastname=random_string("lastname", 20), nickname=random_string("nicktname", 10),
+            title="Testing", company_name="TestCompany",
+            company_address="Moscow, Sovetskaya str.138",
+            homephone="777-62-48", mobilephone="947-242-952", workphone="826-42-67", fax="927-42-64",
+            email1="test@test.com", email2="test2@test.com", email3="test3@test.com",
+            homepage="www.training.com", bday="12", bmonth="June", byear="1983",
+            aday="16", amonth="September", address2="Sovetskaya,138", homephone2="535-25-63")
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="IVO",
-                      title="Testing", company_name="TestCompany",
-                      company_address="Moscow, Sovetskaya str.138",
-                      homephone="777-62-48", mobilephone="947-242-952", workphone="826-42-67", fax="927-42-64",
-                      email1="test@test.com", email2="test2@test.com", email3="test3@test.com",
-                      homepage="www.training.com", bday="12", bmonth="June", byear="1983",
-                      aday="16", amonth="September", address2="Sovetskaya,138", homephone2="535-25-63")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
